5bda7cb0b24e72e3171519691e5ef0299fa576f5
박찬준 <ch4njun@wemakeprice.com>
2021-09-14 23:31:41 +0900

    Project Initialize

---
+++ src/main/java/com/pangtudy/conferenceapi/ConferenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.pangtudy.conferenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pangtudy/conferenceapi/ConferenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pangtudy.conferenceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
