79fef50bd0fce4320d0e0bb16e8f3319349005e8
slolee <slolee@naver.com>
2022-06-01 14:28:50 +0900

    #0 Stage 환경용 application-stg.yml 파일 추가

--- src/main/java/com/pangtudy/conferenceapi/core/config/EurekaInstanceConfig.java
+++ src/main/java/com/pangtudy/conferenceapi/core/config/EurekaInstanceConfig.java
@@ -5,9 +5,7 @@ import com.netflix.appinfo.AmazonInfo;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class EurekaInstanceConfig {
 	@Bean
 	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
